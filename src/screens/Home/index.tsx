047b94dd2397583977ddd933f83506b89107b3d4
Douglas Lisboa <douglasfernandolisboaa@gmail.com>
2023-09-04 08:43:36 -0300

    Commit inicial

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,22 @@
+import { Text, TextInput, View } from 'react-native';
+import {styles } from './styles';
+
+export default function Home() {
+  return (
+    <View style={ styles.container }>
+      <Text style={ styles.eventName }> 
+        Nome do evento
+    </Text>
+    <Text style={ styles.eventDate }>
+        Sexta, 4 de Novembro de 2023
+    </Text>
+
+    <TextInput
+        style={styles.input}
+        placeholder='Nome do participante'
+        placeholderTextColor='#6b6b6b'
+    />
+
+    </View>
+  )
+}
---
+++ src/screens/Home/styles.ts
@@ -0,0 +1,26 @@
+import { StyleSheet } from 'react-native';
+
+export const styles =  StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#131016',
+      padding: 24
+    },
+    eventName: {
+      color:'#FDFCFE',
+      fontSize: 24,
+      fontWeight: 'bold'
+    },
+    eventDate: {  
+      color:'#6b6b6b',
+      fontSize: 16
+    },
+    input: {
+        height: 56,
+        backgroundColor: '#1F1E25',
+        borderRadius: 5,
+        color: '#FFF',
+        padding: 16,
+        fontSize: 16
+    }
+  });
